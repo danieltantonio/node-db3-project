5695c6185138b8872b504a610e144dedf08df45c
Daniel Antonio <dernlsenpai@gmail.com>
2020-10-07 12:58:00 -0700

    Created SQL Queries

--- queries.sql
+++ queries.sql
@@ -2,8 +2,29 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT Product.ProductName, Category.CategoryName
+FROM Product
+JOIN Category ON Product.CategoryId = Category.Id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+SELECT 'Order'.Id, Shipper.CompanyName
+FROM 'Order'
+JOIN Shipper ON 'Order'.ShipVia = Shipper.Id
+WHERE 'Order'.OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT p.ProductName, o.Quantity
+FROM OrderDetail AS o
+JOIN Product AS p
+ON p.Id = o.ProductId
+WHERE o.OrderId = 10251;
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT OrderDetail.OrderId, Customer.CompanyName AS CustomerCompany, Employee.LastName AS EmployeeLastName
+FROM 'Order'
+JOIN OrderDetail ON 'Order'.id = OrderDetail.OrderId
+JOIN Customer ON 'Order'.CustomerId = Customer.Id
+JOIN Employee ON 'Order'.EmployeeId = Employee.Id;
